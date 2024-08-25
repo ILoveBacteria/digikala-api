647750e4a832a0f0125f23b2170b1efcffa0e0bd
Moein Arabi <moein.mo81@gmail.com>
2024-08-25 15:31:09 +0330

    Add get product detail function

--- src/digikala_api/request.py
+++ src/digikala_api/request.py
@@ -1,6 +1,11 @@
 import requests
-from digikala_api.product import Product
 
+from digikala_api.error import ProductNotFoundError, HTTPError
+from digikala_api.base import Product, Url
+
+
+API_URL = Url('https://api.digikala.com/v2/')
+WEBSITE_URL = Url('https://www.digikala.com/')
 
 def product_detail(product_id: str) -> Product:
     """
@@ -12,6 +17,21 @@ def product_detail(product_id: str) -> Product:
     Returns:
         Product: A Product object that contains product detail
     """
-    url = f'https://api.digikala.com/v2/product/{product_id}/'
-    response = requests.get(url)
-    return response.json()
+    url = API_URL / 'product' / product_id
+    response = requests.get(str(url))
+    data = response.json()
+    if response.status_code != 200:
+        raise HTTPError(response.status_code)
+    if data['status'] == 404:
+        raise ProductNotFoundError(product_id)
+    if data['status'] != 200:
+        raise HTTPError(data['status'])
+    product = data['data']['product']
+    return Product(
+        id=product['id'], 
+        english_name=product['title_en'], 
+        persian_name=product['title_fa'],
+        url=str(WEBSITE_URL / product['url']['uri']),
+        image_url=product['images']['main']['url'][0],
+        price=product['default_variant']['price']['selling_price'],
+    )
